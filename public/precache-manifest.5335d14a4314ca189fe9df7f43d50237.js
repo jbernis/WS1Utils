5ece83f5ec1916b5f8c8d4e399cb17227afd564a
Jean-Loup Bernis <jeanloupbernis@gmail.com>
2020-07-31 16:56:28 +0200

    add frontend

---
+++ public/precache-manifest.5335d14a4314ca189fe9df7f43d50237.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "564d5d9f826f09606c0774e93cc2827b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "970b58840f16f1dffc8e",
+    "url": "/static/css/2.71916e70.chunk.css"
+  },
+  {
+    "revision": "972bcbc14ef5c834c84d",
+    "url": "/static/css/main.2c7e67e8.chunk.css"
+  },
+  {
+    "revision": "970b58840f16f1dffc8e",
+    "url": "/static/js/2.2cffbd59.chunk.js"
+  },
+  {
+    "revision": "972bcbc14ef5c834c84d",
+    "url": "/static/js/main.e008af89.chunk.js"
+  },
+  {
+    "revision": "b32108dd1c28731828a6",
+    "url": "/static/js/runtime-main.738565b7.js"
+  },
+  {
+    "revision": "29bf9ca84638f384d63510af0a8d9d95",
+    "url": "/static/media/gen1.29bf9ca8.png"
+  },
+  {
+    "revision": "eb386f70447bc113caf0acae8dba5671",
+    "url": "/static/media/gen2.eb386f70.png"
+  },
+  {
+    "revision": "f81be9933e4fd13ad7b2ff8ecd92a76f",
+    "url": "/static/media/gen2grapg.f81be993.png"
+  },
+  {
+    "revision": "33f4576b1c953b8ec52c6f7f94adaee2",
+    "url": "/static/media/googleapistorage.33f4576b.png"
+  },
+  {
+    "revision": "5f48cfb27df97f5604fcdc6a95a1fc41",
+    "url": "/static/media/intelli.5f48cfb2.png"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "1008c547c8bfc98808c185b221f4a3cf",
+    "url": "/static/media/logo_jlb.1008c547.png"
+  },
+  {
+    "revision": "20cbd480edd75dd476f3d16c6a763f28",
+    "url": "/static/media/sea.20cbd480.jpg"
+  },
+  {
+    "revision": "2bc3dfb116a2f55987d61e84b621f431",
+    "url": "/static/media/serviceacc.2bc3dfb1.png"
+  }
+]);
